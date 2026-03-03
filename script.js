31aa59310f49d0d984dd101c2a1a58f5c35ae5ed
kylienguyenn <165774086+kylienguyenn@users.noreply.github.com>
2026-03-03 20:53:55 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Listen for a click event on the "Shop on TikTok" button
+document.querySelector('button').addEventListener('click', function() {
+    alert("Redirecting to TikTok Shop...");
+});
